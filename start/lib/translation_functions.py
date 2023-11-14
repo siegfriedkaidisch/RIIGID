e9d83234f5c70e52a64ad6c8df4f62e5b88d9be9
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2023-11-14 16:27:03 +0100

    backup

---
+++ start/lib/translation_functions.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 13:49:28 2023
+
+@author: kaidisch_siegfried
+"""
+import numpy as np
+
+from ase import Atom, Atoms
+
+######################################################################################################################
+
+def get_force_mol(mol_indices, f):
+    '''
+    Get the net force acting on the molecule.
+
+    Inputs:
+        mol_indices: list of length n_atoms_in_molecule
+            List containing indices of the molecule's atoms in "full"
+        f: np.ndarray of shape (n_atoms_in_full_system, 3)
+            Forces acting on the atoms in "full" (in eV/Angstroem)
+
+    Returns:
+        np.ndarray of shape (3,)
+            Net force acting on the molecule (in eV/Angstroem)
+    '''
+    mol_f = f[mol_indices]
+    f_center = np.sum(mol_f, axis=0)
+    return f_center
+
+######################################################################################################################
+
+def translate_mol(mol, f_center, stepsize_trans_x, stepsize_trans_y, stepsize_trans_z):
+    '''
+    Translate molecule in direction of the applied force
+
+    Inputs:
+        mol: ase.atoms.Atoms
+            The molecule to be translated
+        f_center: np.ndarray of shape (3,) or list of length 3
+            Net force acting on the molecule (in eV/Angstroem)
+        stepsize_trans_x/y/z: number
+            Timesteps; usually all three should have the same value; (in Dalton*Angstroem**2/eV)
+
+    Returns:
+        np.ndarray of shape (n_atoms_in_molecule,3)
+            The positions (in Angstroem) of the molecule's atoms after the transformation
+    '''
+    mol_mass = np.sum(mol.get_masses())
+    for atom in mol:
+        atom.position[0] += stepsize_trans_x * f_center[0]/mol_mass
+        atom.position[1] += stepsize_trans_y * f_center[1]/mol_mass
+        atom.position[2] += stepsize_trans_z * f_center[2]/mol_mass
+    return mol.positions.copy()
+
+######################################################################################################################
---
+++ start/rigid_molecule_on_rigid_surface_dominik.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 13:49:28 2023
+
+Local geometry optimization of rigid molecule on top of rigid surface
+
+@author: kaidisch_siegfried
+"""
+import numpy as np
+import pickle
+import time
+
+from ase import Atom, Atoms
+from ase.build import add_adsorbate, make_supercell, sort, add_vacuum
+from ase.visualize import view
+from ase.io import read
+from ase.io.vasp import write_vasp
+from ase.constraints import FixAtoms, FixBondLengths
+from ase.io.vasp import read_vasp
+import os
+from ase.calculators.vasp.vasp import Vasp
+from ase.optimize import BFGS, MDMin
+from ase.optimize.sciopt import SciPyFminBFGS, SciPyFminCG
+
+from lib.rotation_functions import add_rot_props_mol, rotate_mol2, get_inertia_mat_and_inv, get_torque_mol
+from lib.translation_functions import get_force_mol
+from lib.transrot_functions import random_step_mol, transrot_mol
+from lib.boundary_functions import apply_boundaries_mol
+from lib.misc_functions import get_mol_indices, copy_mol
+    
+t1 = time.time()
+
+######################################################################################################################
+##########################                      Settings                         #####################################
+######################################################################################################################
+
+# General settings
+middle_height = 8.5 #in Angstroem, used to separate molecule and surface
+vacuum = 10 # in Angstroem, total height of vacuum
+
+# Settings for rigid geometry optimization
+max_rigid_steps = 3 # maximal number of rigid optimization steps
+pos_conv = 0.00000000001 # in Angstroem, stop rigid optimization if all atoms of the molecule move less than pos_conv (Warning: may coverge due to small stepsizes rather than actual convergence)
+max_step_0 = 0.1 #max allowed change of position of an atom (translation+rotation) in iteration 0
+max_step = 0.1 #max allowed change of position of an atom (translation+rotation) in iterations 1+
+stepsize_factor_up = 1.2 # increase stepsize by this factor, if last iteration successfully lowered energy
+stepsize_factor_dn = 0.2 # decrease stepsize by this factor, if last iteration led to higher energy
+
+# Flags to control motion
+do_trans_x = 0
+do_trans_y = 0
+do_trans_z = 0
+z_only_rot = True
+
+# Calculator settings for rigid optimization
+rigid_opt_vasp_settings = {
+    'directory':'./rigid_opt/',
+    'txt':"out",
+
+    'istart':1,
+    'npar':4,
+
+    'lorbit':11,
+    'nwrite':2,
+    'lvhar':True,
+    'lwave':True,
+    'lasph':True,
+
+    'isif':2,
+    'isym':0,
+
+    'prec':'Accu',
+    'encut':300,
+    'ediff':1e-5,
+    'nelmin':4,
+    'nelm':300,
+    'algo':'Fast',
+
+#    'enaug':644.9,
+
+    'xc':'PBE',
+    'ivdw':12,
+    'ismear':0,
+    'sigma':0.05,
+
+    'idipol':3,
+    'ldipol':True,
+
+    'ispin':2,
+    'magmom':[0.0]*48 + [0.0]*16 + [0.0]*16 + [-1.5]*2,
+#    'icharg':1,
+
+    'nsw':0, # leave this at zero!
+
+    'ldau':True,
+    'ldautype':2,
+    'ldaul':[-1, -1, -1, 2],
+    'ldauu':[0.0, 0.0, 0.0, 3.0],
+    'ldauj':[0.0]*4,
+    'ldauprint':1,
+    'lmaxmix':4,
+
+    'kpts':[1, 1, 1],
+    'gamma':True,  # Gamma-centered k-mesh
+
+    'setups':"recommended"
+}
+
+######################################################################################################################
+##########################           Prepare Geometry Optimization               #####################################
+######################################################################################################################
+
+def get_mol_indices2(full, mol, cutoff=1e-4):
+    mol_indices = []
+    for mol_atom in mol:
+        for full_atom in full:
+            if np.linalg.norm(mol_atom.position - full_atom.position) < cutoff:
+                mol_indices.append(full_atom.index)
+    return mol_indices
+
+# Import Geometry
+full = read_vasp(file='./POSCAR_Dominik')
+
+# Remove substrate to speed up tests
+full = full[full.positions[:,2]>middle_height]
+
+mol0 = read_vasp(file='./POSCAR_Dominik_mol0')
+mol1 = read_vasp(file='./POSCAR_Dominik_mol1')
+mol2 = read_vasp(file='./POSCAR_Dominik_mol2')
+mol3 = read_vasp(file='./POSCAR_Dominik_mol3')
+molecules = [mol0,mol1,mol2,mol3]
+for mol in molecules:
+    add_rot_props_mol(mol)
+
+# Control initial geometry
+#mol_indices = get_mol_indices(full=full[~(full.symbols=='Ni')], middle_height=middle_height)
+mol_indices = [get_mol_indices2(full=full, mol=mol) for mol in molecules]
+del full[mol_indices]
+full += mol0.copy()
+full += mol1.copy()
+full += mol2.copy()
+full += mol3.copy()
+
+# Sort atoms
+full = sort(full)
+
+# Fix vacuum height
+existing_vac = full.get_cell_lengths_and_angles()[2] - np.max(full.positions[:,2]) + np.min(full.positions[:,2])
+add_vacuum(full, vacuum-existing_vac)
+
+# Export POSCAR for documentation purposes
+write_vasp('./POSCAR_start', full, direct=True, vasp5=True, sort=True)
+
+######################################################################################################################
+##########################          RIGID Geometry Optimization                  #####################################
+######################################################################################################################
+
+# Set up VASP calculator (determines INCAR, KPOINTS and POTCAR)
+calculator = Vasp(**rigid_opt_vasp_settings)
+
+# Set calculator
+full.set_calculator(calculator)
+
+# Optimize location and rotation of rigid molecule on rigid surface
+stepsize = 100 # the value set here doesn't matter, it's just for the first test step, so it will be changed anyway
+count_bad_steps = 0 # needed to cope with bad iterations (where energy got larger)
+mol_list = []
+pos_list = []
+mol_inertia_list = []
+mol_inertia_inv_list = []
+rot_list = []
+energy_list = []
+f_center_list = []
+t_center_list = []
+for i in range(max_rigid_steps):
+    print('-----')
+    print('Iteration ',i)
+    #####################################################################
+    # Save for later
+    mol_list.append([copy_mol(mol) for mol in molecules])
+
+    # Get indices of the atoms forming the molecule
+    mol_indices = [get_mol_indices2(full=full, mol=mol) for mol in molecules]
+
+    # Save center of mass of current geometry
+    mol_com = [mol.get_center_of_mass() for mol in molecules]
+    pos_list.append(mol_com)
+
+    # Get inertia matrix and inverse
+    tmp = np.array([get_inertia_mat_and_inv(mol=full[mol_indices_i]) for mol_indices_i in mol_indices])
+    mol_inertia = tmp[:,0].copy()
+    mol_inertia_inv = tmp[:,1].copy()
+    del tmp
+    mol_inertia_list.append(mol_inertia)
+    mol_inertia_inv_list.append(mol_inertia_inv)
+
+    # Save rotation angles of current geometry
+    rot_list.append([mol.euler_angles for mol in molecules])
+    
+    # Run one SCF cycle
+    f = full.get_forces(apply_constraint=False)
+    e = full.get_potential_energy(force_consistent=True)
+
+    # Save energy of current geometry
+    energy_list.append(e)
+
+    # Get net force on molecule
+    f_center = [get_force_mol(mol_indices=mol_indices_i, f=f) for mol_indices_i in mol_indices]
+    f_center_list.append(f_center)
+
+    # get torque on molecule
+    t_center = [get_torque_mol(full=full, mol_indices=mol_indices_i, f=f) for mol_indices_i in mol_indices]
+    t_center_list.append(t_center)
+
+    #####################################################################
+
+    if i == 0:
+        None
+    else: # Check if energy got smaller
+        if energy_list[-1] < energy_list[-2]:
+            # If the prev step successfully lowered the energy, continue as normal and try a bigger stepsize this time
+            count_bad_steps = 0
+            stepsize *= stepsize_factor_up
+        else:
+            # Last step led to higher energy -> forget last step and continue from 2nd last step with smaller stepsize
+            count_bad_steps +=1  # needed to handle situation of two consecutive bad iterations
+            stepsize *= stepsize_factor_dn 
+            molecules = [copy_mol(mol) for mol in mol_list[-1 - count_bad_steps]]
+            f_center = f_center_list[-1 - count_bad_steps]
+            mol_inertia_inv = mol_inertia_inv_list[-1 - count_bad_steps]
+            t_center = t_center_list[-1 - count_bad_steps]
+    print('Test Stepsize: ' + str(stepsize))
+
+    # Prepare movement of molecule
+    del full[mol_indices]
+    mol_pos_old = [mol.positions.copy() for mol in molecules]
+
+    # Test step to find max displacement of atoms
+    molecules_test = [copy_mol(mol) for mol in molecules]
+    mol_pos_diff_max = -1
+    for j in range(len(molecules)):
+        mol_pos_new = transrot_mol(mol=molecules_test[j], f_center=f_center[j], stepsize_trans_x=stepsize*do_trans_x, stepsize_trans_y=stepsize*do_trans_y, stepsize_trans_z=stepsize*do_trans_z, 
+                                            mol_inertia_inv=mol_inertia_inv[j], t_center=t_center[j], stepsize_rot=stepsize, z_only_rot=z_only_rot)
+        mol_pos_diff = np.abs(mol_pos_new - mol_pos_old[j])
+        mol_pos_diff_max = np.max([np.max(np.linalg.norm(mol_pos_diff, axis=1)), mol_pos_diff_max])
+    del molecules_test
+
+    # Initialize stepsize or adapt it to too large atomic displacements
+    if i==0: # In the first iteration we determine the initial stepsize, such that a step of max_step is performed
+        factor = max_step_0/mol_pos_diff_max
+    else: # In later iterations, calc factor to prevent too large atomic deplacements
+        if mol_pos_diff_max>max_step:
+            factor = max_step/mol_pos_diff_max
+        else:
+            factor = 1.0
+    stepsize *= factor
+    print('Actual Stepsize: ' + str(stepsize))
+
+    # Move molecule and get new positions
+    mol_pos_new = []
+    for j in range(len(molecules)):
+        mol_pos_new_i = transrot_mol(mol=molecules[j], f_center=f_center[j], stepsize_trans_x=stepsize*do_trans_x, stepsize_trans_y=stepsize*do_trans_y, stepsize_trans_z=stepsize*do_trans_z, 
+                                        mol_inertia_inv=mol_inertia_inv[j], t_center=t_center[j], stepsize_rot=stepsize, z_only_rot=z_only_rot)
+        mol_pos_new.append(mol_pos_new_i)
+
+    for mol in molecules:
+        full += mol
+
+    #####################################################################
+
+    # Print some stuff for testing purposes
+    #print('Displacement/stepsize: ' + str(f_center/np.sum(mol.get_masses())))
+    #print('Angle/stepsize: ' + str(np.linalg.norm(mol_inertia_inv@t_center) * (180/np.pi)))
+    #print('-----')
+
+    # Save geometry
+    write_vasp('./rigid_opt/CONTCAR_'+str(i), full, direct=True, vasp5=True, sort=True)
+    
+    # Check for convergence
+    mol_pos_diff = np.abs(np.array(mol_pos_new) - np.array(mol_pos_old))
+    mol_pos_diff = np.linalg.norm(mol_pos_diff, axis=2) 
+    if np.max(mol_pos_diff) < pos_conv:
+        print('-----')
+        break
+
+
+######################################################################################################################
+##########################                 Finish the job                        #####################################
+######################################################################################################################
+
+# Export optimization data from rigid and vasp optim
+optim_data = {'position': pos_list, 'rotation': rot_list, 'energy': energy_list, 'force': f_center_list, 'torque': t_center_list, 'inertia':mol_inertia_list, 'inertia_inv':mol_inertia_inv_list, 'mol':mol_list}
+f = open('optim_data.pk','wb')
+pickle.dump(optim_data, f)
+f.close()
+
+print("\n")
+print('///////////////////////////////////////////////////////')
+print('Summary of Optimization:')
+print('Energy:')
+print(optim_data['energy'])
+print('Position:')
+print(optim_data['position'])
+print('Force:')
+print(optim_data['force'])
+print('Torque:')
+print(optim_data['torque'])
+print('Rotation')
+print(optim_data['rotation'])
+
+print("\n")
+print('Final Geometry:')
+print("Energy: "+str(optim_data['energy'][-1]))
+print("Position: "+str(optim_data['position'][-1]))
+print("Rotation: "+str(optim_data['rotation'][-1]))
+print("Force: "+str(optim_data['force'][-1]))
+print("Torque: "+str(optim_data['torque'][-1]))
+
+print("\n")
+print('Duration / h:')
+print((time.time()-t1)/3600)
+print('///////////////////////////////////////////////////////')
