ca1fcac6c03b3270afb15a6c5aeae5d729bd134e
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2023-11-14 09:57:34 +0100

    Update misc_functions.py

--- lib/misc_functions.py
+++ lib/misc_functions.py
@@ -21,11 +21,15 @@ def copy_mol(mol):
 
 ######################################################################################################################
 
-def get_mol_indices(full, middle_height):
+def get_mol_indices(full, middle_height, above=True):
     '''
     Given the full system ("full"=surface+molecule), find the indices of the molecule's atoms. 
     To do so, specifiy a height ("middle_height") separating the surface from the molecule. 
-    I.e.: Atoms below middle_height are considered to belong to the surface and atoms above middle_height 
+
+    above=True: Atoms below middle_height are considered to belong to the surface and atoms above middle_height 
+    are considered to belong to the molecule.
+
+    above=False: Atoms above middle_height are considered to belong to the surface and atoms below middle_height 
     are considered to belong to the molecule.
 
     Inputs:
@@ -33,11 +37,17 @@ def get_mol_indices(full, middle_height):
             The full system (surface+molecule) under study
         middle_height: number
             Height (in Angstroem) used to separate molecule from surface (see description above)
+        above: Bool
+            See explanation given above
 
     Returns:
         list of length n_atoms_in_molecule
             List containing indices of the molecule's atoms in "full"
     '''
-    return [ atom.index for atom in full if atom.position[2] > middle_height ]
+    if above:
+        return [ atom.index for atom in full if atom.position[2] >= middle_height ]
+    else:
+        return [ atom.index for atom in full if atom.position[2] < middle_height ]
+
 
 ######################################################################################################################
