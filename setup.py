422440cbd48e4313c14f0740a9a487dc2a5a1208
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2023-12-21 16:42:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Geometry optimization of rigid molecules on rigid surfaces.",
     packages=find_packages(),
     license="MIT",
-    python_requires=">3.8.0",
+    python_requires=">3.6.0",
     url="https://github.com/siegfriedkaidisch/RIGID",
     author="Siegfried Kaidisch",
     author_email="siegfried.kaidisch@uni-graz.at",
