86b34b315f1a007e80c71a44b408766db1c2fabe
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2024-01-10 17:24:20 +0100

    Updated demos

--- demo/Cu(O)_benzene/main_minimal.py
+++ demo/Cu(O)_benzene/main_minimal.py
@@ -1,20 +1,17 @@
-"""Example of a RIGID geometry optimization
+"""Minimal example of a RIGID geometry optimization
 
 """
 
-from ase.calculators.vasp.vasp import Vasp
 from ase.io.vasp import read_vasp
 
 from rigid import RIGID
-from rigid.convergence import CC_Displacement
 from rigid.library import get_atoms_indices_by_height
-from rigid.optimizer import GDWAS
 
 # User defines full system
 atoms = read_vasp(file="./POSCAR_start")
 
 # User instantiates a RIGID calculation object using an ASE atoms object of the full system
-rigid = RIGID(atoms=atoms, name="example")
+rigid = RIGID(atoms=atoms, name="minimal_example")
 
 # User defines a fragment using the molecule's coordinates and defines what kind of motion is allowed
 # Maybe at some point additional ways of defining fragments... e.g. by adding an additional atoms object
@@ -26,7 +23,7 @@ rigid.define_fragment_by_indices(
     indices=molecule_indices, allowed_translation="xy", allowed_rotation="z"
 )
 
-# User sets up the ASE calculator and its settings
+# User defines the calculator and its settings
 vasp_settings = {
     "directory": "./vasp/",
     "txt": "out",
@@ -63,29 +60,7 @@ vasp_settings = {
     "gamma": True,  # Gamma-centered k-mesh
     "setups": "recommended",
 }
-calculator = Vasp(**vasp_settings)
-rigid.set_calculator(calculator)
-
-# User sets the RIGID optimizer and its settings
-optimizer_settings = {
-    "stepsize_factor_up": 1.2,
-    "stepsize_factor_dn": 0.2,
-    "max_step": 0.1,
-    "max_step_0": 0.01,
-    "start_with_random_step": True,
-    "displacement_r0": 0.01,
-    "angle_r0": 0.1,
-    "respect_restrictions_r0": False,
-    "seed_r0": 1234,
-    "max_iter": 100
-}
-optimizer = GDWAS(**optimizer_settings)  # gradient descent with adaptive stepsize
-rigid.set_optimizer(optimizer)
-
-# User sets the convergence criterion
-convergence_settings = {"cutoff": 0.0001}
-convergence1 = CC_Displacement(**convergence_settings)
-rigid.set_convergence_criterion(convergence1)
+rigid.set_calculator(calculator="Vasp", settings=vasp_settings)
 
 # Start the rigid optimization
 rigid.run()
