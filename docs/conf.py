095e43fa3c86bc4e2764ffc8803a58e08210659a
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2024-01-12 15:20:37 +0100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ release = '1.0.0a0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc","sphinx_rtd_theme"]
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc","sphinx_rtd_theme"]
+#extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
 
 
 templates_path = ['_templates']
@@ -29,5 +29,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
