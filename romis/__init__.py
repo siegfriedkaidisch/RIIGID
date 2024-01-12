fa6e5bc5773c3377bc8bb6c3b6f0c7958c8e7f34
siegfriedkaidisch <80325973+siegfriedkaidisch@users.noreply.github.com>
2024-01-12 11:53:54 +0100

    Update __init__.py

--- romis/__init__.py
+++ romis/__init__.py
@@ -1,3 +1,20 @@
+"""
+ROMIS
+=====
+
+Rigid Optimization of Molecular Interface Structures
+----------------------------------------------------
+
+Introduction to ROMIS: https://github.com/siegfriedkaidisch/ROMIS
+
+Detailed explanations on how to use ROMIS are given in the docstrings within the Code.
+
+Author: Siegfried Kaidisch (siegfried.kaidisch(at)uni-graz.at)
+
+"""
+
+
+
 from romis.fragment import Fragment
 from romis.optimization_step import OptimizationStep
 from romis.romis import ROMIS
